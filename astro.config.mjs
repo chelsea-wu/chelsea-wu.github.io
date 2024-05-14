b201ddc89c22efe9d5ea1d8f74d45519f702dc13
Evan Howie <etshowie@gmail.com>
2024-05-14 18:39:04 -0400

    deployable prolly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+const production = import.meta.env.PROD;
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: production ? "https://chelsea-wu.github.io" : undefined,
+  base: production ? "/new-website" : undefined,
+});
