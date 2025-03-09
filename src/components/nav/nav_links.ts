b685ac0fcdde4a3c3f029323743289c9a933b8e3
Evan Howie <etshowie@gmail.com>
2025-03-09 18:58:58 -0400

    change some things:

---
+++ src/components/nav/nav_links.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Projects",
+    href: "/projects",
+  },
+  {
+    title: "Gallery",
+    href: "/gallery",
+  },
+  {
+    title: "Resume",
+    href: "/resume.pdf",
+  },
+];
--- src/components/projects/projects.ts
+++ src/components/projects/projects.ts
@@ -1,26 +1,28 @@
+import { type ImageMetadata } from "astro";
+
 export type Project = {
-    title: string;
-    href: string;
-    description: string;
-    tags: string[];
-    imgPath: string;
-}
-      
+  title: string;
+  href: string;
+  description: string;
+  tags: string[];
+  img: Promise<Record<string, ImageMetadata>>;
+};
 
 export const projects: Project[] = [
-    {
-        title: "Soft Soap",
-        description: "Re-branding assignment to emphasize on longevity and adapting to current trends while still maintaining the youthful vibrancy that the brand currently has",
-        href: "/projects/soft-soap",
-        tags: ["Adobe Illustrator", "Figma", "Procreate"],
-        imgPath: "/src/assets/projects/soft-soap.png",
-    },
-    {
-        title: "Escape",
-        description: "Incorporating augmented reality into graphic design to help address the rise of mental health challenges in undergraduate students, by creating our own solution.",
-        href: "/projects/escape",
-        tags: ["Figma", "Research", "Augmented Reality"],
-        imgPath: "/src/assets/projects/escape.png",
-    }
-]
-    
+  {
+    title: "Soft Soap",
+    description:
+      "Re-branding assignment to emphasize on longevity and adapting to current trends while still maintaining the youthful vibrancy that the brand currently has",
+    href: "/projects/soft-soap",
+    tags: ["Adobe Illustrator", "Figma", "Procreate"],
+    img: import("/src/assets/projects/soft-soap.png"),
+  },
+  {
+    title: "Escape",
+    description:
+      "Incorporating augmented reality into graphic design to help address the rise of mental health challenges in undergraduate students, by creating our own solution.",
+    href: "/projects/escape",
+    tags: ["Figma", "Research", "Augmented Reality"],
+    img: import("/src/assets/projects/escape.png"),
+  },
+];
